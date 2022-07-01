32c8bf18e74ee76404a2a1f7a69144cad0c6e4fc
Bruxism <bruxism@sina.com>
2022-07-01 09:07:18 +0800

    添加一个样例

--- src/main/java/org/luvsa/vary/Cache.java
+++ src/main/java/org/luvsa/vary/Cache.java
@@ -1,9 +1,9 @@
 package org.luvsa.vary;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
@@ -12,40 +12,24 @@ import java.util.function.Function;
  * @author Aglet
  * @create 2022/6/29 9:56
  */
-public abstract class Cache<T, R extends Provider<T>> {
+public final class Cache<T, R extends Provider<T>> {
 
     private final Map<Class<?>, R> map = new HashMap<>();
 
-    private BiConsumer<Class<R>, BiConsumer<Class<?>, R>> initiator;
+    private final Consumer<BiConsumer<Class<?>, R>> initiator;
+
+    public Cache(Consumer<BiConsumer<Class<?>, R>> initiator) {
+        this.initiator = initiator;
+    }
 
     public Function<T, ?> get(Class<?> clazz) {
         if (map.isEmpty()) {
-            @SuppressWarnings("unchecked")
-            var cls = (Class<R>) getParameterizedType(1);
-            initiator.accept(cls, map::put);
+            initiator.accept(map::put);
         }
         var provider = map.get(clazz);
         if (provider == null) {
-            throw new UnsupportedOperationException("不支持由 [" + getParameterizedType(0) + "] -> [" + clazz + "] 的转换！");
+            return null;
         }
         return provider.get(clazz);
-    }
-
-    private Class<?> getParameterizedType(int index) {
-        var aClass = getClass();
-        var generic = aClass.getGenericSuperclass();
-        //Parameterized Type
-        if (generic instanceof ParameterizedType type) {
-            var arguments = type.getActualTypeArguments();
-            var argument = arguments[index];
-            if (argument instanceof Class<?> clazz) {
-                return clazz;
-            }
-        }
-        throw new RuntimeException(aClass + " 没有泛型参数！");
-    }
-
-    public void register(BiConsumer<Class<R>, BiConsumer<Class<?>, R>> initiator){
-        this.initiator = initiator;
     }
 }
---
+++ src/main/java/org/luvsa/vary/FunctionManager.java
@@ -0,0 +1,49 @@
+package org.luvsa.vary;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * @author Aglet
+ * @create 2022/6/30 15:30
+ */
+public abstract class FunctionManager<T, R extends Provider<T>> extends Manager<Function<T, ?>> {
+
+    protected final Cache<T, R> map = new Cache<>(this::init);
+
+    private final BiFunction<T, Class<?>, ?> error = (o, clz) -> {
+        throw new UnsupportedOperationException("不支持 [" + o.getClass() + " : " + o + "] -> [" + clz + "]");
+    };
+
+    @Override
+    protected Function<T, ?> offer(Class<?> clazz) {
+        var function = map.get(clazz);
+        if (function == null) {
+            return next(clazz);
+        }
+        return function;
+    }
+
+    protected Function<T, ?> next(Class<?> clazz) {
+        return item -> error.apply(item, clazz);
+    }
+
+    private void init(BiConsumer<Class<?>, R> initiator) {
+        var aClass = getClass();
+        var generic = aClass.getGenericSuperclass();
+        //Parameterized Type
+        if (generic instanceof ParameterizedType type) {
+            var arguments = type.getActualTypeArguments();
+            var argument = arguments[1];
+            if (argument instanceof Class) {
+                @SuppressWarnings("unchecked")
+                var clazz = (Class<R>) argument;
+                load(clazz, initiator);
+                return;
+            }
+        }
+        throw new RuntimeException(aClass + " 没有泛型参数！");
+    }
+}
