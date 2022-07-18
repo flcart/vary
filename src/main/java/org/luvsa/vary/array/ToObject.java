545cec5e3691c18f5055d23759ed908ace3c8683
Bruxism <bruxism@sina.com>
2022-07-18 13:32:14 +0800

    添加数组转数组的实现

---
+++ src/main/java/org/luvsa/vary/array/ToObject.java
@@ -0,0 +1,27 @@
+package org.luvsa.vary.array;
+
+import org.luvsa.vary.TypeSupplier.Types;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.function.Function;
+
+/**
+ * @author Aglet
+ * @create 2022/7/18 11:11
+ */
+@Types(Object[].class)
+public class ToObject implements AProvider {
+
+    @Override
+    public Function<Object, ?> get(Type type) {
+        return o -> {
+            var length = Array.getLength(o);
+            var array = new Object[length];
+            for (int i = 0; i < length; i++) {
+                array[i] = Array.get(o, i);
+            }
+            return array;
+        };
+    }
+}
