36c1dd88e20075ab3465e4e6d3257a4a2301af2a
Aglet <498971746@qq.com>
2023-03-22 09:58:44 +0800

    添加拦截器

--- web/src/main/java/org/luvsa/web/Interceptor.java
+++ web/src/main/java/org/luvsa/web/Interceptor.java
@@ -7,5 +7,10 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
  * @create 2023/3/21 14:23
  */
 public interface Interceptor {
+    /**
+     * 添加拦截器
+     *
+     * @param request 请求
+     */
     void accept(ServerHttpRequest request);
 }
